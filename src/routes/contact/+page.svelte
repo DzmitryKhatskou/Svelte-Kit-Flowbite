21726a92df3bbcdd95f020eb2ead8fb43588f4d8
Dzmitry Khatskou <khatskov@gmail.com>
2023-05-17 19:53:02 +0300

    changes

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,56 +1,54 @@
 <script>
-  let name = "";
-  let email = "";
-  let message = "";
+	let name = '';
+	let email = '';
+	let message = '';
 
-  function handleSubmit() {
-    // Add your logic for handling form submission here
-    console.log("Submitting form...");
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Message:", message);
+	function handleSubmit() {
+		// Add your logic for handling form submission here
+		console.log('Submitting form...');
+		console.log('Name:', name);
+		console.log('Email:', email);
+		console.log('Message:', message);
 
-    // Additional actions such as sending data to a server
-    // can be added here or within the handleSubmit() function
-  }
+		// Additional actions such as sending data to a server
+		// can be added here or within the handleSubmit() function
+	}
 </script>
 
-<div class="max-w-md mx-auto">
-  <h2 class="text-2xl font-bold mb-4">Contact Me</h2>
-  <form class="space-y-4" on:submit|preventDefault={handleSubmit}>
-    <div>
-      <label class="block mb-2" for="name">Name</label>
-      <input
-        class="w-full border border-gray-300 rounded p-2"
-        type="text"
-        id="name"
-        bind:value={name}
-        required
-      />
-    </div>
-    <div>
-      <label class="block mb-2" for="email">Email</label>
-      <input
-        class="w-full border border-gray-300 rounded p-2"
-        type="email"
-        id="email"
-        bind:value={email}
-        required
-      />
-    </div>
-    <div>
-      <label class="block mb-2" for="message">Message</label>
-      <textarea
-        class="w-full border border-gray-300 rounded p-2"
-        id="message"
-        bind:value={message}
-        required
-      />
-    </div>
-    <div>
-      <button class="bg-blue-500 text-white rounded px-4 py-2" type="submit"
-        >Submit</button
-      >
-    </div>
-  </form>
+<div class="w-full md:w-2/3 lg:w-1/2">
+	<h2 class="text-2xl font-bold mb-4">Contact Me</h2>
+	<form class="space-y-4" on:submit|preventDefault={handleSubmit}>
+		<div>
+			<label class="block mb-2" for="name">Name</label>
+			<input
+				class="w-full border border-gray-300 rounded p-2"
+				type="text"
+				id="name"
+				bind:value={name}
+				required
+			/>
+		</div>
+		<div>
+			<label class="block mb-2" for="email">Email</label>
+			<input
+				class="w-full border border-gray-300 rounded p-2"
+				type="email"
+				id="email"
+				bind:value={email}
+				required
+			/>
+		</div>
+		<div>
+			<label class="block mb-2" for="message">Message</label>
+			<textarea
+				class="w-full border border-gray-300 rounded p-2"
+				id="message"
+				bind:value={message}
+				required
+			/>
+		</div>
+		<div>
+			<button class="bg-blue-500 text-white rounded px-4 py-2" type="submit">Send</button>
+		</div>
+	</form>
 </div>
