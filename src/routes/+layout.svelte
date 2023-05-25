d26349def508974bf82a04d1f671d9efd0d91e63
DzmitryKhatskou <khatskov@gmail.com>
2023-05-25 22:51:27 +0300

    main mb

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <Navbar />
 <DarkMode class="fixed top-[68px] md:top-20 left-2" />
 <main
-	class="flex flex-col py-20 mt-[60px] md:mt-[72px] justify-center items-center max-w-[90%] px-10
+	class="flex xl:mb-[181px] md:mb-[165px] mb-[133px] flex-col py-20 mt-[60px] md:mt-[72px] justify-center items-center max-w-[90%] px-10
 	  mx-auto"
 >
 	<slot />
